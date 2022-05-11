cd16fbd2409864022526b3a595a0198d66cd65a4
Declan Davis <dexthefish@gmail.com>
2022-05-11 17:26:05 -0400

    refactoring. moved connect() to a separate file called client.

---
+++ client.js
@@ -0,0 +1,20 @@
+const net = require('net');
+const connect = function() {
+  const conn = net.createConnection({
+    host: '165.227.47.243', // IP address
+    port: '50541', // PORT number
+  });
+  conn.setEncoding("utf8");
+  conn.on("connect", () => {
+    console.log("Connection successful!")
+  });
+  conn.on("data", (data) => {
+    console.log(data);
+  });
+  conn.on("end", () => {
+    console.log("Connection terminated.")
+  });
+
+  return conn;
+};
+module.exports = { connect };
--- play.js
+++ play.js
@@ -1,23 +1,5 @@
-const net = require('net');
+const { connect } = require("./client");
 
-const connect = function() {
-  const conn = net.createConnection({
-    host: '165.227.47.243', // IP address
-    port: '50541', // PORT number
-  });
-  conn.setEncoding("utf8");
-  conn.on("connect", () => {
-    console.log("Connection successful!")
-  });
-  conn.on("data", (data) => {
-    console.log(data);
-  });
-  conn.on("end", () => {
-    console.log("Connection terminated.")
-  });
-
-  return conn;
-};
 
 console.log("Connecting...");
 connect();
